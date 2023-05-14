290197ff72c7abb8215e742a136ef1f9b1633507
soniagaber <soniagaber90@gmail.com>
2023-05-14 21:46:39 +0200

    creata tabella Trains e relativa Migration

--- database/migrations/2023_05_14_174840_create_trains_table.php
+++ database/migrations/2023_05_14_174840_create_trains_table.php
@@ -15,6 +15,18 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+
+            //Azienda Stazione di partenza Stazione di arrivo Orario di partenza Orario di arrivo Codice Treno Numero Carrozze In orario Cancellato
+            $table->string('azienda', 50);
+            $table->string('stazione_di_partenza', 50);
+            $table->string('stazione_di_arrivo', 50);
+            $table->dateTime('orario_di_partenza');
+            $table->dateTime('orario_di_arrivo');
+            $table->string('codice_treno', 8);
+            $table->tinyInteger('numero_carrozze');
+            $table->boolean('in_orario');
+            $table->boolean('cancellato')->default(false);
+
             $table->timestamps();
         });
     }
